f39556c34b5ed8d2f9c94ac262e6c48faafb892e
kuddai <vkuddai92@yandex.ru>
2015-11-09 04:22:56 +0000

    task 1.2.a is ready

--- asg1/code/tsk2.m
+++ asg1/code/tsk2.m
@@ -1,1 +1,7 @@
 %load imgregdata.mat % I do it via terminal
+n_offset = 5000;
+x_left   = xtr_nf(1:n_offset, 1032);
+x_above  = xtr_nf(1:n_offset, 1032 - 34);
+x_target = ytr_nf(1:n_offset);
+
+scatter3(x_left, x_above, x_target);
